3dd87b9b9b4477a975831fb08d7e770969998ccd
Gabriel Depotte <depott_g@epita.fr>
2019-05-16 18:57:18 +0200

    [ADMIN] Added the admin page

--- StatViz/models.py
+++ StatViz/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
 class Project(models.Model):
-    id = models.CharField(max_length=32)
+    id = models.CharField(max_length=32, primary_key=True)
     name = models.CharField(max_length=128)
     description = models.CharField(max_length=2048)
 
+    def __str__(self):
+        return self.name
+
 class Task(models.Model):
-    id = models.CharField(max_length=32)
+    id = models.CharField(max_length=32, primary_key=True)
     name = models.CharField(max_length=128)
     nb = models.IntegerField()
     start_date = models.DateTimeField()
@@ -15,8 +18,14 @@ class Task(models.Model):
     real_duration = models.DurationField()
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
+
 class Pause(models.Model):
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
     name = models.CharField(max_length=128)
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
