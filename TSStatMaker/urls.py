f4848ba8a444d75075636231d99e95d24a7af911
Gabriel Depotte <depott_g@epita.fr>
2019-05-16 12:28:12 +0200

    [STATVIZ] Added an empty dashboard.html and linked it to the app

--- TSStatMaker/urls.py
+++ TSStatMaker/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from StatViz import views as sv_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', sv_views.index, name='index'),
 ]
