2fe20cffc93872e038c2e0f054dbe16a3c9b4b3c
Gabriel Depotte <depott_g@epita.fr>
2019-05-16 18:26:24 +0200

    [DATABASE] Made a first version of the models

--- StatViz/models.py
+++ StatViz/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    id = models.CharField(max_length=32)
+    name = models.CharField(max_length=128)
+    description = models.CharField(max_length=2048)
+
+class Task(models.Model):
+    id = models.CharField(max_length=32)
+    name = models.CharField(max_length=128)
+    nb = models.IntegerField()
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    duration = models.DurationField()
+    real_duration = models.DurationField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+class Pause(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    name = models.CharField(max_length=128)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
