6288a76722bced17dc39ea04ef034cb961072ca0
Gabriel Depotte <depott_g@epita.fr>
2019-05-19 17:32:55 +0200

    [VIEW] The front part of /projects/ seems working

--- StatViz/views.py
+++ StatViz/views.py
@@ -20,7 +20,8 @@ def index(request):
 def line_per_project(request):
     tasks = Task.objects.all()
     projects = []
-    # Building the projects list with their names and
+
+    # Building the list of the projects names
     for task in tasks:
         placed = False
         for project in projects:
@@ -30,7 +31,22 @@ def line_per_project(request):
         if not placed:
             projects.append(task.project_name)
 
+    # Building as an histogram all the dates
+
+    # Fake data to be sent
+    dated_tasks = [
+        [datetime.date(2019, 5, 13), "240", "60", "0"],
+        [datetime.date(2019, 5, 14), "350", "75", "0"],
+        [datetime.date(2019, 5, 15), "400", "0", "0"],
+        [datetime.date(2019, 5, 16), "380", "40", "180"],
+        [datetime.date(2019, 5, 17), "120", "120", "60"],
+        [datetime.date(2019, 5, 18), "0", "60", "120"],
+        [datetime.date(2019, 5, 19), "60", "60", "60"],
+    ]
+    projects = ["EPITA", "RIX", "Petits jeux & programmes"]
+
     context = {
-        "projects": projects
+        "projects": projects,
+        "dated_tasks": dated_tasks
     }
     return render(request, 'StatViz/line_per_project.html', context)
