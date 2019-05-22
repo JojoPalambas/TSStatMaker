f35428a6551558f3d071e7e4eb54561d6f81579b
Gabriel Depotte <depott_g@epita.fr>
2019-05-22 15:46:33 +0200

    [BACK] Modularized line_per_project

--- StatViz/views.py
+++ StatViz/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
 from db import init as init_db
+import StatViz.views_utils as vu
 import datetime
 
 from StatViz.models import Task
 
+# TODO Make the user enable/disable the "ALL" project
+# TODO Make the user change the start and the end of the chart
 
 def init(request):
     context = {}
@@ -18,57 +21,22 @@ def index(request):
 
 
 def sub_line_per_project(request, accumulate=False):
+    print(request.GET)
+
     tasks = Task.objects.all()
 
     # Building the list of the projects names and getting the first and last dates
-    projects = ["ALL"]
-    first_date = datetime.datetime.now().date()
-    last_date = datetime.date(1970, 1, 1)
-    for task in tasks:
-        # Project-listing part
-        placed = False
-        for project in projects:
-            if project == task.project_name:
-                placed = True
-                continue
-        if not placed:
-            projects.append(task.project_name)
-
-        # Date extremums part
-        if task.start_date < first_date:
-            first_date = task.start_date
-        if task.start_date > last_date:
-            last_date = task.start_date
+    projects, first_date, last_date = vu.projects_list(tasks)
 
     # Building as an histogram all the dates
-    histogram = []
-    current_date = first_date
-    while current_date <= last_date:
-        new_entry = [current_date]
-        for project in projects:
-            new_entry.append(0)
-        histogram.append(new_entry)
-        current_date = current_date + datetime.timedelta(days=1)
+    histogram = vu.generate_empty_histogram(first_date, last_date, len(projects))
 
     # Filling the histogram
-    for task in tasks:
-        # Finds the line
-        for entry in histogram:
-            if entry[0] == task.start_date:
-                # Finds the column
-                for i in range(len(projects)):
-                    if projects[i] == task.project_name:
-                        # Conversion to minutes
-                        entry[1] += (task.duration.seconds + 24*3600*task.duration.days) / 60
-                        entry[i + 1] += (task.duration.seconds + 24*3600*task.duration.days) / 60
-                        continue
-                continue
+    histogram = vu.fill_histogram(histogram, tasks, projects)
 
     # Accumulating the values if needed
     if accumulate:
-        for i in range(len(histogram) - 1):
-            for j in range(len(histogram[i + 1]) - 1):
-                histogram[i + 1][j + 1] += histogram[i][j + 1]
+        histogram = vu.accumulate_histogram(histogram)
 
     context = {
         "projects": projects,
---
+++ StatViz/views_utils.py
@@ -0,0 +1,58 @@
+import datetime
+
+
+def projects_list(tasks):
+    projects = ["ALL"]
+    first_date = datetime.datetime.now().date()
+    last_date = datetime.date(1970, 1, 1)
+    for task in tasks:
+        # Project-listing part
+        placed = False
+        for project in projects:
+            if project == task.project_name:
+                placed = True
+                continue
+        if not placed:
+            projects.append(task.project_name)
+
+        # Date extremums part
+        if task.start_date < first_date:
+            first_date = task.start_date
+        if task.start_date > last_date:
+            last_date = task.start_date
+    return projects, first_date, last_date
+
+
+def generate_empty_histogram(first_date, last_date, projects_nb):
+    histogram = []
+    current_date = first_date
+    while current_date <= last_date:
+        new_entry = [current_date]
+        for i in range(projects_nb):
+            new_entry.append(0)
+        histogram.append(new_entry)
+        current_date = current_date + datetime.timedelta(days=1)
+    return histogram
+
+
+def fill_histogram(histogram, tasks, projects):
+    for task in tasks:
+        # Finds the line
+        for entry in histogram:
+            if entry[0] == task.start_date:
+                # Finds the column
+                for i in range(len(projects)):
+                    if projects[i] == task.project_name:
+                        # Conversion to minutes
+                        entry[1] += (task.duration.seconds + 24*3600*task.duration.days) / 60
+                        entry[i + 1] += (task.duration.seconds + 24*3600*task.duration.days) / 60
+                        continue
+                continue
+    return histogram
+
+
+def accumulate_histogram(histogram):
+    for i in range(len(histogram) - 1):
+        for j in range(len(histogram[i + 1]) - 1):
+            histogram[i + 1][j + 1] += histogram[i][j + 1]
+    return histogram
