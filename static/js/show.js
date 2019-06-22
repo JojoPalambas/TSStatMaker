b5acc432c22fe4f1f77603f7a90ba609425758d8
Gabriel Depotte <depott_g@epita.fr>
2019-06-23 01:08:48 +0200

    [FRONT] Added Month granularity

--- static/js/show.js
+++ static/js/show.js
@@ -1,5 +1,3 @@
-console.log("OPEN show.js");
-
 let tasksPersistance = null;
 
 function dateToString(date) {
@@ -28,7 +26,6 @@ function loadChart(tasks) {
 }
 
 function groupTasksByDate(tasks) {
-    console.log("Group tasks by date");
     const dates = [];
 
     let startDate = new Date(tasks[0].startDate);
@@ -62,39 +59,60 @@ function groupTasksByDate(tasks) {
         }
     }
 
-    console.log(dates);
     return dates;
 }
 
 function applyGranularity(dates) {
-    console.log("Apply granularity");
-    const ret = [];
+    let ret = [];
 
-    console.log(ret);
-    return dates;
+    const granularity = document.getElementById("granularity-select").value;
+    if (granularity === "day") {
+        // Do nothing
+        ret = dates;
+    }
+    else if (granularity === "Week") {
+        ret = dates;
+    }
+    else if (granularity === "Month") {
+        for (let i = 0; i < dates.length; i++) {
+            const index = ret.findIndex(function(row) {return row.date.getMonth() === dates[i].date.getMonth()});
+            if (index === -1) {
+                ret.push({
+                    date: new Date(dates[i].date.getFullYear(), dates[i].date.getMonth() + 1, 0),
+                    tasks: dates[i].tasks
+                });
+            }
+            else {
+                ret[index].tasks = ret[index].tasks.concat(dates[i].tasks);
+            }
+        }
+    }
+    else if (granularity === "Year") {
+    }
+    else {
+        console.log("BUG");
+        // Do nothing
+        ret = dates;
+    }
+
+    return ret;
 }
 
 function applyDateFilters(dates) {
-    console.log("Apply date filters");
     const ret = [];
 
-    console.log(ret);
     return dates;
 }
 
 function applyProjectFilter(dates) {
-    console.log("Apply project filter");
     const ret = [];
 
-    console.log(ret);
     return dates;
 }
 
 function applyTaskFilter(dates) {
-    console.log("Apply task filter");
     const ret = [];
 
-    console.log(ret);
     return dates;
 }
 
@@ -105,10 +123,8 @@ function durationToHours(duration) {
 }
 
 function accumulateByDisplayMode(dates) {
-    console.log("Accumulate by display mode");
     const ret = accumulateByProject(dates);
 
-    console.log(ret);
     return ret;
 }
 
@@ -165,12 +181,11 @@ function drawCurveTypes() {
     data = applyTaskFilter(data);
 
     // Makes the data digest for the chart, regarding the display mode
-    var columnsRows = accumulateByDisplayMode(data);
+    const columnsRows = accumulateByDisplayMode(data);
 
     const dataTable = new google.visualization.DataTable();
 
     // Adding the columns
-    console.log(columnsRows);
     dataTable.addColumn('date', "Moment");
     for (let i = 0; i < columnsRows.columns.length; i++) {
         dataTable.addColumn('number', columnsRows.columns[i]);
@@ -178,27 +193,6 @@ function drawCurveTypes() {
 
     // Adding the rows
     dataTable.addRows(columnsRows.rows);
-
-    /*
-    dataTable.addColumn('number', 'X');
-    dataTable.addColumn('number', 'Dogs');
-    dataTable.addColumn('number', 'Cats');
-
-    dataTable.addRows([
-        [0, 0, 0],    [1, 10, 5],   [2, 23, 15],  [3, 17, 9],   [4, 18, 10],  [5, 9, 5],
-        [6, 11, 3],   [7, 27, 19],  [8, 33, 25],  [9, 40, 32],  [10, 32, 24], [11, 35, 27],
-        [12, 30, 22], [13, 40, 32], [14, 42, 34], [15, 47, 39], [16, 44, 36], [17, 48, 40],
-        [18, 52, 44], [19, 54, 46], [20, 42, 34], [21, 55, 47], [22, 56, 48], [23, 57, 49],
-        [24, 60, 52], [25, 50, 42], [26, 52, 44], [27, 51, 43], [28, 49, 41], [29, 53, 45],
-        [30, 55, 47], [31, 60, 52], [32, 61, 53], [33, 59, 51], [34, 62, 54], [35, 65, 57],
-        [36, 62, 54], [37, 58, 50], [38, 55, 47], [39, 61, 53], [40, 64, 56], [41, 65, 57],
-        [42, 63, 55], [43, 66, 58], [44, 67, 59], [45, 69, 61], [46, 69, 61], [47, 70, 62],
-        [48, 72, 64], [49, 68, 60], [50, 66, 58], [51, 65, 57], [52, 67, 59], [53, 70, 62],
-        [54, 71, 63], [55, 72, 64], [56, 73, 65], [57, 75, 67], [58, 70, 62], [59, 68, 60],
-        [60, 64, 56], [61, 60, 52], [62, 65, 57], [63, 67, 59], [64, 68, 60], [65, 69, 61],
-        [66, 70, 62], [67, 72, 64], [68, 75, 67], [69, 80, 72]
-    ]);
-    */
 
     const options = {
         height: 1000,
